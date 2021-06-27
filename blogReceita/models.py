001368f6771d20be9bcefac714252aae62101a6e
Amanda <amanda.simoes1707@gmail.com>
2021-06-27 13:41:07 -0300

    Model de Receita

--- blogReceita/models.py
+++ blogReceita/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Receita(models.Model):
+    nome = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    preparo = models.TextField()
+    porcao = models.PositiveIntegerField()
+    tempoPreparo = models.CharField(max_length=100)
+    publicacao = models.DateTimeField(auto_now=True)
+    infoAdicional = models.TextField()
